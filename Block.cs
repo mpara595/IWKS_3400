741770e818096528f2c669a6d4319ff56b0def99
mpara595 <mpara595@live.kutztown.edu>
2017-02-20 18:11:59 -0700

    Create Block.cs

---
+++ Block.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;   //   for Texture2D
+using Microsoft.Xna.Framework;  //  for Vector2
+
+
+namespace IWKS_3400_Lab4
+{
+    class Block
+    {
+        public Texture2D texture { get; set; } //  block texture, read-only property
+        public Vector2 position { get; set; }  //  block position on screen
+        public Vector2 size { get; set; }      //  block size in pixels
+        public Vector2 velocity { get; set; }  //  block velocity
+        private Vector2 screenSize { get; set; } //  screen size
+
+
+        public Block(Texture2D newTexture, Vector2 newPosition, Vector2 newSize, int ScreenWidth, int ScreenHeight)
+        {
+            texture = newTexture;
+            position = newPosition;
+            size = newSize;
+            screenSize = new Vector2(ScreenWidth, ScreenHeight);
+            
+        } 
+        public void move(Ball ball)
+        {
+                //go down until position + size.Y hits the bottom of the screen
+                do
+            {
+                position += new Vector2(0f, -12f);
+            } while(this.position.Y + this.size.Y < this.screenSize.Y);
+
+        }
+        public void reverse()
+        {
+            do
+            {  //go up until your position + size reaches the top of the screen
+                position += new Vector2(0f, 12f);
+            } while (this.position.Y + this.size.Y != this.screenSize.Y);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
+    }
+}
